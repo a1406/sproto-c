e4c577c0d6eed005555f28cdd38e735bd7b3c556
jacktang <jack at zzfs>
2016-08-29 20:25:19 +0800

    1

--- readlua/test.c
+++ readlua/test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
+#include "../sproto.h"
+#include "../sprotoc_common.h"
+#include "test.h"
 
 int traverse_table(lua_State *L)
 {
@@ -49,65 +55,30 @@ int traverse_table(lua_State *L)
 	return 0;
 }
 
-void test(lua_State *n)
+int traverse_main_table(lua_State *L)
 {
-	int type;
-	lua_State *L;
-	L = luaL_newstate();
-	luaL_openlibs(L);
-	
-	lua_newtable(L);
-	type = lua_type(L, -1);
-	printf("%d: %d %s\n", -1, type, lua_typename(L, type));	
-	
-		//依次将各个元素放入table
-	lua_pushnumber(L,1);
-	lua_pushstring(L, "AA");
-	lua_rawset(L,1);
-
-	lua_pushnumber(L,2);
-	lua_pushstring(L, "BB");
-	lua_rawset(L,1);
-
-	lua_pushnumber(L,3);
-	lua_newtable(L);
-	lua_pushstring(L, "CC");
-	lua_rawseti(L,-2,1);
-
-	lua_pushstring(L, "DD");
-	lua_rawseti(L,-2,2);
-	lua_rawset(L,1);
-
-	lua_setglobal(L,"ta");
-		//此时栈中为空，此处省略其他操作
-		//将ta压入栈顶
-	lua_getglobal(L, "ta");
-		//获得第一个元素
-	lua_rawgeti(L, 1,1);
-	if(lua_type(L,-1) == LUA_TSTRING)
-		printf("%s", lua_tostring(L,-1));
-	lua_pop(L,1);
-
-	lua_rawgeti(L, 1,2);
-	if(lua_type(L,-1) == LUA_TSTRING)
-		printf("%s", lua_tostring(L,-1));
-	lua_pop(L,1);
-
-	lua_rawgeti(L, 1,3);
-	if(lua_type(L,-1) == LUA_TTABLE)
+	lua_pushnil(L);
+	    // 现在的栈：-1 => nil; index => table
+	int index = -2;
+	while (lua_next(L, -2))
 	{
-			//因为第三个元素是table，所以继续获得它的第一个元素
-		lua_rawgeti(L, -1,1);
-		if(lua_type(L,-1) == LUA_TSTRING)
-			printf("%s", lua_tostring(L,-1));
-		lua_pop(L,1);
+			// 现在的栈：-1 => value; -2 => key; index => table
+			// 拷贝一份 key 到栈顶，然后对它做 lua_tostring 就不会改变原始的 key 值了
+		lua_pushvalue(L, -2);
+			// 现在的栈：-1 => key; -2 => value; -3 => key; index => table
 
-		lua_rawgeti(L, -1,2);
-		if(lua_type(L,-1) == LUA_TSTRING)
-			printf("%s", lua_tostring(L,-1));
-		lua_pop(L,1);
+		double key = lua_tonumber(L, -1);
+		printf("key = %d\n", (int)key);
+		lua_pop(L, 1);
+
+		int type = lua_type(L, -1);
+		assert(type == LUA_TTABLE);
+
+		traverse_table(L);
+		
+		lua_pop(L, 1);
 	}
-	lua_pop(L,1); //此时栈顶为ta
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -116,16 +87,6 @@ int main(int argc, char *argv[])
 	luaL_openlibs(L);
 	luaL_dofile(L, argv[1]);
 
-//	lua_getglobal(L,"ScriptTable");
-
-	int type;
-	type = lua_type(L, -1);
-	printf("%d: %d %s\n", -1, type, lua_typename(L, type));
-
-//	lua_rawgeti(L, 1, 170000001);
-//	type = lua_type(L, -1);
-//	printf("%d: %d %s\n", -1, type, lua_typename(L, type));	
-	
-	traverse_table(L);
+	traverse_main_table(L);
     return 0;
 }
